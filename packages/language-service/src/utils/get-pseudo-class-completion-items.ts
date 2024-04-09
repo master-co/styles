9e0ae7f6d52de0763b2bfd3eedf734b747c7d5e0
Aron <i@aron.tw>
2024-04-09 16:20:22 +0800

    Add(Language Service): Hint pseudo-class and pseudo-element selectors

--- packages/language-service/src/utils/get-pseudo-class-completion-items.ts
+++ packages/language-service/src/utils/get-pseudo-class-completion-items.ts
@@ -1,29 +1,65 @@
-import { MasterCSS } from '@master/css'
+import { MasterCSS, generateCSS } from '@master/css'
 import cssDataProvider from './css-data-provider'
-import type { CompletionItem, CompletionItemKind } from 'vscode-languageserver-protocol'
+import { type CompletionItem, CompletionItemKind } from 'vscode-languageserver-protocol'
 import sortCompletionItems from './sort-completion-items'
+import { IPseudoClassData } from 'vscode-css-languageservice'
+import { getCSSDataDocumentation } from './get-css-data-documentation'
 
-const insertTextMode = 1
-const kind: CompletionItemKind = 3
+const kind = CompletionItemKind.Function
 
-export default function getPseudoClassCompletionItems(css: MasterCSS, sign = ''): CompletionItem[] {
-    const completionItems = cssDataProvider.providePseudoClasses()
+export default function getPseudoClassCompletionItems(css: MasterCSS = new MasterCSS(), syntax: string): CompletionItem[] {
+    const pseudoClassDataList = cssDataProvider.providePseudoClasses()
+        .filter((data) => {
+            // exclude @page pseudo-classes
+            if ([':first', ':left', ':right', ':blank'].includes(data.name)) return false
+            return true
+        })
+    const completionItems = pseudoClassDataList
         .map((data) => {
             // fix https://github.com/microsoft/vscode-custom-data/issues/78
             const name = /:(?:dir|has|is|nth-col|where)/.test(data.name) ? data.name + '()' : data.name
             let sortText = name.startsWith(':-')
-                ? 'zz' + name.slice(2)
-                : name.replace(/^:/, '')
-            if (sortText.endsWith('()')) sortText = 'z' + sortText
+                ? 'yyyy' + name.slice(2)
+                : 'yy' + name.replace(/^:/, '')
+            if (sortText.endsWith('()')) sortText = 'y' + sortText
             return {
                 label: name,
-                insertText: name.slice(sign.length),
                 sortText,
-                documentation: data.description,
-                insertTextMode,
+                documentation: getCSSDataDocumentation(data, {
+                    generatedCSS: generateCSS([syntax + name.slice(1)], css),
+                    docs: 'selectors'
+                }),
                 kind,
                 data
             } as CompletionItem
         })
+
+    for (const selectorName in css.config.selectors) {
+        if (selectorName.startsWith('::')) continue
+        const selectorValue = css.config.selectors[selectorName]
+        const name = selectorName.endsWith('(') ? selectorName + ')' : selectorName
+        const value = typeof selectorValue === 'string'
+            ? selectorValue.endsWith('(') ? selectorValue + ')' : selectorValue
+            : selectorValue
+        const data: IPseudoClassData | undefined = pseudoClassDataList.find((data) =>
+            typeof value === 'string' ? value.startsWith(data.name) : false)
+        let sortText = name.startsWith(':-')
+            ? 'yyyy' + name.slice(2)
+            : 'yy' + name.replace(/^:/, '')
+        if (sortText.endsWith('()')) sortText = 'y' + sortText
+        const completionItem: CompletionItem = {
+            label: name,
+            documentation: getCSSDataDocumentation(data, {
+                generatedCSS: generateCSS([syntax + name.slice(1)], css),
+                docs: 'selectors'
+            }),
+            sortText,
+            kind,
+            detail: String(value),
+            data
+        }
+        completionItems.push(completionItem)
+    }
+
     return sortCompletionItems(completionItems)
 }
--- packages/language-service/src/utils/get-pseudo-element-completion-items.ts
+++ packages/language-service/src/utils/get-pseudo-element-completion-items.ts
@@ -1,29 +1,60 @@
-import { MasterCSS } from '@master/css'
+import { MasterCSS, generateCSS } from '@master/css'
 import cssDataProvider from './css-data-provider'
-import type { CompletionItem, CompletionItemKind } from 'vscode-languageserver-protocol'
+import { type CompletionItem, CompletionItemKind } from 'vscode-languageserver-protocol'
 import sortCompletionItems from './sort-completion-items'
+import { getCSSDataDocumentation } from './get-css-data-documentation'
+import { IPseudoElementData } from 'vscode-css-languageservice'
 
-const insertTextMode = 1
-const kind: CompletionItemKind = 3
+const kind = CompletionItemKind.Function
 
-export default function getPseudoElementCompletionItems(css: MasterCSS, sign = ''): CompletionItem[] {
-    const completionItems = cssDataProvider.providePseudoElements()
+export default function getPseudoElementCompletionItems(css: MasterCSS = new MasterCSS(), syntax: string): CompletionItem[] {
+    const pseudoElementDataList = cssDataProvider.providePseudoElements()
+    const completionItems = pseudoElementDataList
         .map((data) => {
             // fix https://github.com/microsoft/vscode-custom-data/issues/78
             const name = /::(?:part|slotted)/.test(data.name) ? data.name + '()' : data.name
             let sortText = name.startsWith('::-')
-                ? 'zz' + name.slice(3) // 'z' to put it at the end
-                : name.replace(/^::/, '')
+                ? 'zzzz' + name.slice(3)
+                : 'zz' + name.replace(/^::/, '')
             if (sortText.endsWith('()')) sortText = 'z' + sortText
             return {
                 label: name,
-                insertText: name.slice(sign.length),
-                sortText: 'zzzz' + sortText,
-                documentation: data.description,
-                insertTextMode,
+                sortText,
+                documentation: getCSSDataDocumentation(data, {
+                    generatedCSS: generateCSS([syntax + name.slice(2)], css),
+                    docs: 'selectors'
+                }),
                 kind,
                 data
             } as CompletionItem
         })
+
+    for (const selectorName in css.config.selectors) {
+        if (!selectorName.startsWith('::')) continue
+        const selectorValue = css.config.selectors[selectorName]
+        const name = selectorName.endsWith('(') ? selectorName + ')' : selectorName
+        const value = typeof selectorValue === 'string'
+            ? selectorValue.endsWith('(') ? selectorValue + ')' : selectorValue
+            : selectorValue
+        const data: IPseudoElementData | undefined = pseudoElementDataList.find((data) =>
+            typeof value === 'string' ? value.startsWith(data.name) : false)
+        let sortText = name.startsWith('::-')
+            ? 'zzzz' + name.slice(3)
+            : 'zz' + name.replace(/^::/, '')
+        if (sortText.endsWith('()')) sortText = 'z' + sortText
+        const completionItem: CompletionItem = {
+            label: name,
+            documentation: getCSSDataDocumentation(data, {
+                generatedCSS: generateCSS([syntax + name.slice(2)], css),
+                docs: 'selectors'
+            }),
+            sortText,
+            kind,
+            detail: String(value),
+            data
+        }
+        completionItems.push(completionItem)
+    }
+
     return sortCompletionItems(completionItems)
 }
