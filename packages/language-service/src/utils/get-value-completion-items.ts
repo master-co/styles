64f544d403a3822fe502c01014347294f70d65f8
Aron <i@aron.tw>
2024-04-15 17:17:16 +0800

    Fix animation hints

--- packages/language-service/src/utils/get-value-completion-items.ts
+++ packages/language-service/src/utils/get-value-completion-items.ts
@@ -88,25 +88,26 @@ export default function getValueCompletionItems(css: MasterCSS = new MasterCSS()
                     completionItems.push(completionItem)
                 }
             }
+        }
 
-            /**
-             * @example animation:fade
-             */
-            if (EachRule.definition?.sign && EachRule.definition.includeAnimations) {
-                for (const animationName in css.animations) {
-                    const isNative = EachRule.definition.layer && [Layer.Native, Layer.NativeShorthand].includes(EachRule.definition.layer)
-                    completionItems.push({
-                        label: animationName + '|1s',
-                        kind: CompletionItemKind.Value,
-                        documentation: getCSSDataDocumentation(undefined, {
-                            generatedCSS: generateCSS([ruleKey + ':' + animationName + '|1s'], css),
-                            docs: isCoreRule(EachRule.id) && EachRule.id
-                        }),
-                        detail: isNative ? EachRule.id + ': ' + animationName + '|1s' : animationName + '|1s'
-                    })
-                }
+        /**
+         * @example animation:fade
+         */
+        if (EachRule.keys.includes(ruleKey) && EachRule.definition.includeAnimations) {
+            for (const animationName in css.animations) {
+                const isNative = EachRule.definition.layer && [Layer.Native, Layer.NativeShorthand].includes(EachRule.definition.layer)
+                completionItems.push({
+                    label: animationName,
+                    kind: CompletionItemKind.Value,
+                    documentation: getCSSDataDocumentation(undefined, {
+                        generatedCSS: generateCSS([ruleKey + ':' + animationName], css),
+                        docs: isCoreRule(EachRule.id) && EachRule.id
+                    }),
+                    detail: isNative ? EachRule.id + ': ' + animationName : animationName
+                })
             }
         }
+
         /**
          * Ambiguous values
          * @example text: -> center, left, right, justify
