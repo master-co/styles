b410b60520fbadd39135d5174f8a414acaf02d8c
Aron <i@aron.tw>
2024-04-22 20:04:03 +0800

    Update import paths for css-shared package

--- packages/extractor.vite/tests/nuxt.js/dev.test.ts
+++ packages/extractor.vite/tests/nuxt.js/dev.test.ts
@@ -1,10 +1,10 @@
 import fs from 'fs'
 import path from 'path'
-import cssEscape from 'css-shared/utils/css-escape'
+import cssEscape from '@master/css-shared/utils/css-escape'
 import puppeteer, { type Browser, type Page } from 'puppeteer-core'
-import { copy } from 'css-shared/utils/fs'
+import { copy } from '@master/css-shared/utils/fs'
 import { SpawndChildProcess, spawnd } from 'spawnd'
-import waitForDataMatch from 'css-shared/utils/wait-for-data-match'
+import waitForDataMatch from '@master/css-shared/utils/wait-for-data-match'
 
 const examplePath = path.join(__dirname, '../../../../examples/nuxt.js-with-static-extraction')
 const tmpDir = path.join(__dirname, 'tmp/dev')
