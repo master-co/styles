d9e64f3a54f324736eab3479c236d41a6a111133
Aron <i@aron.tw>
2024-04-22 18:32:22 +0800

    Update code-linting component styles and ESLint configuration

--- website/app/[locale]/(root)/docs/eslint/metadata.ts
+++ website/app/[locale]/(root)/docs/eslint/metadata.ts
@@ -1,9 +1,7 @@
 import { Metadata } from 'websites/types/Metadata'
 
 const metadata: Metadata = {
-    title: {
-        absolute: 'Master CSS ESLint'
-    },
+    title: 'ESLint',
     description: 'The ESLint configuration and plugin reference for Master CSS.',
     category: 'API Reference'
 }
