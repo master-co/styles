3ba5147bc0c3a0d2ad3f739ca513eb94c4bc9d63
Aron <i@aron.tw>
2024-04-25 22:40:20 +0800

    Update Jest configuration and add new dependencies

--- packages/eslint-config/jest.config.ts
+++ packages/eslint-config/jest.config.ts
@@ -1,10 +1,13 @@
 // import { pathsToModuleNameMapper } from 'ts-jest'
-// import { getTsconfig } from 'get-tsconfig'
+import { getTsconfig } from 'get-tsconfig'
+// import { resolve } from 'path'
 
 // const tsconfig = getTsconfig()?.config
 
 /** @type {import('jest').Config} */
 export default {
     preset: '@techor/jest',
-    // moduleNameMapper: tsconfig?.compilerOptions?.paths && pathsToModuleNameMapper(tsconfig.compilerOptions?.paths, { prefix: '<rootDir>/' }),
+    // moduleNameMapper: tsconfig?.compilerOptions?.paths && pathsToModuleNameMapper(tsconfig.compilerOptions?.paths, {
+    //     prefix: resolve(tsconfig?.compilerOptions?.baseUrl || '.')
+    // }),
 }
