711fe1b96e772132ebd5a40a97b83f4e66020185
Aron <i@aron.tw>
2024-05-21 19:23:46 +0800

    Deprecate jest in favor of vitest

--- packages/eslint-plugin/vitest.config.ts
+++ packages/eslint-plugin/vitest.config.ts
@@ -1,7 +1,6 @@
 import { UserWorkspaceConfig, defineConfig } from 'vitest/config'
 import config from '../../shared/vitest.config'
 import extend from '@techor/extend'
-import { } from 'vitest'
 
 export default defineConfig(extend(config, {
     test: {
