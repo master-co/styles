660f5c60d928f5fc6547eae5b0a2af7c164791cf
Aron <i@aron.tw>
2024-05-27 03:58:49 +0800

    Docs: Add Language Server

---
+++ website/app/[locale]/reference/language-server/metadata.ts
@@ -0,0 +1,9 @@
+import { Metadata } from 'websites/types/Metadata'
+
+const metadata: Metadata = {
+    title: 'Language Server',
+    description: 'The language server reference for Master CSS.',
+    category: 'Package'
+}
+
+export default metadata
