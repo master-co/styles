f44e9b31e603846f7bacd6cf1d4b2852e9b13321
Aron <i@aron.tw>
2024-06-02 09:40:32 +0800

    Update configs

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,84 +1,24 @@
 import redirects from './redirects.mjs'
 import CopyPlugin from 'copy-webpack-plugin'
 import path from 'path'
-import withMDX from 'internal/width-mdx.mjs'
 import withWebpackConfig from 'internal/with-webpack-config.mjs'
-import withBundleAnalyzer from 'internal/with-bundle-analyzer.mjs'
+import common from 'internal/common/next.config.mjs'
+import commonHeaders from 'internal/common/headers.mjs'
+import commonRewrites from 'internal/common/rewrites.mjs'
 
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: process.env.VERCEL_CI ? false : true,
-    },
-    experimental: {
-        /**
-         * 解決 JavaScript heap out of memory 問題
-         */
-        webpackBuildWorker: true,
-        /**
-         * 不要啟用。會導致莫名的 MDX 內容解析錯誤，從而無法正確 build
-         */
-        // mdxRs: true
-        /**
-         * 解決：You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file.
-         */
-        externalDir: true
-    },
+let nextConfig = {
+    ...common,
     async redirects() {
         return redirects
     },
     async headers() {
-        return [
-            {
-                source: '/:all*(woff2)',
-                locale: false,
-                headers: [
-                    {
-                        'key': 'Cache-Control',
-                        'value': 'public, max-age=31536000'
-                    }
-                ],
-            },
-        ]
+        return commonHeaders
     },
-    images: {
-        formats: ['image/avif', 'image/webp'],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'avatars.githubusercontent.com'
-            },
-            {
-                protocol: 'https',
-                hostname: 'images.opencollective.com'
-            },
-            {
-                protocol: 'https',
-                hostname: 'img.shields.io'
-            },
-            {
-                protocol: 'https',
-                hostname: 'images.pexels.com'
-            }
-        ],
-    },
-    modularizeImports: {
-        '@tabler/icons-react': {
-            transform: '@tabler/icons-react/dist/esm/icons/{{member}}',
-        }
-    },
-    pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
     async rewrites() {
-        return [
-            {
-                source: '/cdn/:path*',
-                destination: 'https://cdn.jsdelivr.net/npm/@master/:path*',
-            }
-        ]
+        return commonRewrites
     },
     webpack: (config) => {
         config.plugins.push(
@@ -92,6 +32,4 @@ const nextConfig = {
     }
 }
 
-export default withBundleAnalyzer(withMDX(nextConfig), {
-    enabled: process.env.ANALYZE === 'true',
-})
+export default nextConfig
--- website/redirects.mjs
+++ website/redirects.mjs
@@ -1,49 +1,10 @@
-import i18n from 'internal/i18n.config.mjs'
+import { commonRedirects } from '~/internal/next.config.mjs'
 
-const generateRedirect = (source, dest) => ([
-    { source: source, destination: dest, permanent: false },
-    { source: source + '/:path*', destination: dest + '/:path*', permanent: false },
-])
+const redirects = []
 
-const redirects = [
-    // { source: '/sandbox', destination: '/play', permanent: false },
-    // { source: '/sandbox/:path*', destination: '/play/:path*', permanent: false },
-    // { source: '/docs', destination: '/guide', permanent: false },
-    // ...generateRedirect('/docs/installation', '/guide/installation'),
-    // ...generateRedirect('/docs/language-service', '/guide/language-service'),
-    // ...generateRedirect('/docs/code-linting', '/guide/code-linting'),
-    // ...generateRedirect('/docs/migration', '/guide/migration'),
-    // ...generateRedirect('/docs/style-declarations', '/guide/style-declarations'),
-    // ...generateRedirect('/docs/state-selectors', '/guide/state-selectors'),
-    // ...generateRedirect('/docs/rendering-modes', '/guide/rendering-modes'),
-    // ...generateRedirect('/docs/markup-driven', '/guide/markup-driven'),
-    // ...generateRedirect('/docs/global-styles', '/guide/global-styles'),
-    // ...generateRedirect('/docs/browser-support', '/guide/browser-support'),
-    // ...generateRedirect('/docs/responsive-design', '/guide/responsive-design'),
-    // ...generateRedirect('/docs/reusing-styles', '/guide/reusing-styles'),
-    // ...generateRedirect('/docs/variables-and-modes', '/guide/variables-and-modes'),
-    // ...generateRedirect('/docs/dynamic-application', '/guide/dynamic-application'),
-    // ...generateRedirect('/docs/flash-of-unstyled-content', '/guide/flash-of-unstyled-content'),
-    // ...generateRedirect('/docs/lazy-loading', '/guide/lazy-loading'),
-    // ...generateRedirect('/docs/minimization', '/guide/minimization'),
-    // ...generateRedirect('/docs/preload-critical-resources', '/guide/preload-critical-resources'),
-]
-
-export default [
-    { source: `/${i18n.defaultLocale}/:path((?!.*opengraph-image).*)`, destination: '/:path*', permanent: true },
-    ...redirects,
-    ...redirects
-        .map((eachRedirect) => {
-            return i18n.locales
-                .filter((locale) => locale !== 'en')
-                .map((locale) => {
-                    return {
-                        ...eachRedirect,
-                        source: `/${locale}${eachRedirect.source}`,
-                        destination: `/${locale}${eachRedirect.destination}`,
-                    }
-                })
-                .flat()
-        })
-        .flat()
-]
+export default resolve(
+    [
+        commonRedirects,
+        ...redirects
+    ]
+)
