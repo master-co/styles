44629c6b78cec64d113c6098ecb9ff44629210e8
Aron <i@aron.tw>
2024-06-03 04:52:41 +0800

    Rewrite i18n

--- website/middleware.ts
+++ website/middleware.ts
@@ -1,31 +1,9 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import i18n from 'internal/common/i18n.config.mjs'
+import { NextResponse, NextRequest } from 'next/server'
+import rewriteI18n from 'internal/utils/rewrite-i18n'
+import units from './units.json'
 
 export function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname
-    if (
-        // 檢查不包含 opengraph-image
-        pathname.indexOf('opengraph-image-') === -1 &&
-        // 檢查開頭不等於 /tw ...
-        !i18n.locales.find((eachLocale) => pathname.startsWith('/' + eachLocale)) &&
-        // 檢查開頭不等於預設 /en
-        !pathname.startsWith(i18n.defaultLocale)
-    ) {
-        // 將 /* 不等於 locales 的網址一律 rewrites 重寫為 /en/*
-        return NextResponse.rewrite(new URL('/' + i18n.defaultLocale + pathname, request.url))
-    }
+    const response = rewriteI18n(request, units)
+    if (response) return response
     return NextResponse.next()
-}
-
-export const config = {
-    matcher: [
-        '/guide(/.*)?',
-        '/reference(/.*)?',
-        '/play(/.*)?',
-        '/blog(/.*)?',
-        '/roadmap(/.*)?',
-        '/sponsor(/.*)?',
-        '/examples(/.*)?'
-    ],
 }
