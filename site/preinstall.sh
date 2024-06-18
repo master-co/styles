e7f028b8fe8a6e08acb5fd016e74987fc5d69ab3
Aron <i@aron.tw>
2024-06-18 08:33:09 +0800

    Update shell

--- site/preinstall.sh
+++ site/preinstall.sh
@@ -4,11 +4,11 @@
 set -e
 
 if [ -n "$GITHUB_TOKEN" ]; then
-  echo "Running in CI environment, using GITHUB_TOKEN for authentication"
-  # Configure Git to use the GITHUB_TOKEN for authentication
-  git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
+    echo "Running in CI environment, using GITHUB_TOKEN for authentication"
+    # Configure Git to use the GITHUB_TOKEN for authentication
+    git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
 else
-  echo "Running in local environment, using normal authentication"
+    echo "Running in local environment, using normal authentication"
 fi
 
 # Initialize and update submodules
