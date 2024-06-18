c1b62116c1cb2461b47b480553a6e91663ef0c03
Aron <i@aron.tw>
2024-06-18 08:23:06 +0800

    Update scripts

---
+++ site/preinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+if [ -n "$GITHUB_TOKEN" ]; then
+  echo "Running in CI environment, using GITHUB_TOKEN for authentication"
+  # Configure Git to use the GITHUB_TOKEN for authentication
+  git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
+else
+  echo "Running in local environment, using normal authentication"
+fi
+
+# Initialize and update submodules
+git submodule deinit -f --all
+git submodule update --init --recursive
+
+cd ../internal
+git checkout main
+echo ""
