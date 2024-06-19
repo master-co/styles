7b2463b6963a8d98d43b1078a8a15babf13eac43
Aron <i@aron.tw>
2024-06-19 20:37:42 +0800

    Rename Styles to Custom Styles

---
+++ site/app/[locale]/guide/custom-styles/metadata.ts
@@ -0,0 +1,10 @@
+import define from 'internal/utils/metadata'
+
+const metadata = define({
+    title: 'Custom Styles',
+    description: 'A guide to creating abstract styles for your design system.',
+    category: 'Start Building',
+    filename: import.meta.url
+})
+
+export default metadata
