5c5369c59cfd81adfa7dbfefd388263087b23245
Aron <i@aron.tw>
2024-06-20 21:27:48 +0800

    Update references

---
+++ site/app/[locale]/reference/fonts/metadata.ts
@@ -0,0 +1,10 @@
+import define from 'internal/utils/metadata'
+
+const metadata = define({
+    title: 'Fonts',
+    description: 'Customizing fonts for your design system.',
+    category: 'Design Token',
+    filename: import.meta.url
+})
+
+export default metadata
---
+++ site/app/[locale]/reference/spacing-and-sizing/metadata.ts
@@ -0,0 +1,10 @@
+import define from 'internal/utils/metadata'
+
+const metadata = define({
+    title: 'Spacing and Sizing',
+    description: 'Apply consistent sizes across your application.',
+    category: 'Design Token',
+    filename: import.meta.url
+})
+
+export default metadata
