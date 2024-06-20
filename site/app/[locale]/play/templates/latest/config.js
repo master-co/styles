d374d90afd5fbabf105bac7fc5b613aadaf0accf
Aron <i@aron.tw>
2024-06-21 00:01:03 +0800

    Update latest template

--- site/app/[locale]/play/templates/latest/config.js
+++ site/app/[locale]/play/templates/latest/config.js
@@ -1,35 +1,11 @@
 /** @type {import('@master/css').Config} */
 export default {
-    styles: {
-        btn: 'inline-flex center-content px:6x h:48 r:1x bg:primary fg:primary-invert font:14 font:semibold text:center'
-    },
     variables: {
         text: {
             primary: {
-                invert: {
-                    '@light': '$(yellow-90)',
-                    '@dark': '$(yellow-95)'
-                }
+                '@light': '$(yellow-50)',
+                '@dark': '$(amber-20)'
             }
-        },
-        accent: {
-            '@light': '$(yellow-50)',
-            '@dark': '$(amber-20)'
-        },
-        primary: {
-            '@light': '$(yellow-40)',
-            '@dark': '$(yellow-50)'
-        },
-        panel: {
-            '@light': '$(white)',
-            '@dark': '$(gray-80)'
-        },
-        ring: {
-            '@light': '$(slate-40)/.1',
-            '@dark': '$(white)/.1'
-        },
-        'box-shadow': {
-            '01': '0|2|4|black/.12,0|4|8|black/.08,0|20|30|black/.1'
         }
     }
 }
