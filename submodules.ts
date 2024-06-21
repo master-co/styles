0cc523d4818491cc34951fe9bd205e66a5fb3887
Aron <i@aron.tw>
2024-06-21 17:20:30 +0800

    Update animation document

--- submodules.ts
+++ submodules.ts
@@ -3,10 +3,10 @@ import { execSync } from 'child_process'
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 
 if (GITHUB_TOKEN) {
-    console.log("Running in CI environment, using GITHUB_TOKEN for authentication")
+    console.log('Running in CI environment, using GITHUB_TOKEN for authentication')
     execSync(`git config --global url.https://${GITHUB_TOKEN}@github.com/.insteadOf https://github.com`)
 } else {
-    console.log("Running in local environment, using normal authentication")
+    console.log('Running in local environment, using normal authentication')
 }
 
 execSync('git submodule deinit -f --all')
