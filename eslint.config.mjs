53959a7607bdc634607230ed7a6aea7978bf5376
Aron <i@aron.tw>
2024-12-10 12:20:12 +0800

    Add support for .gitignore in ESLint configuration using @eslint/compat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
 import techor from 'eslint-config-techor'
+import { includeIgnoreFile } from '@eslint/compat'
+import { fileURLToPath } from 'node:url'
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
 
 export default [
+    includeIgnoreFile(gitignorePath),
     techor.configs.base,
     techor.configs.stylistic
 ]
