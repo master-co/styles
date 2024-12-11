60c006d4fcaf47b6cefcd283d002d42f7015cb98
Aron <i@aron.tw>
2024-12-12 05:00:58 +0800

    Example: Update Nuxt examples

--- examples/nuxt.js-with-static-extraction/nuxt.config.ts
+++ examples/nuxt.js-with-static-extraction/nuxt.config.ts
@@ -1,6 +1,7 @@
 import CSSExtractorPlugin from '@master/css-extractor.vite'
 
 export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
     vite: {
         plugins: [
             CSSExtractorPlugin()
