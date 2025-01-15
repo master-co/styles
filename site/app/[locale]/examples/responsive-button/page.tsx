11a8f062ce2c181c7d2d5427cd0c7d637d856717
Aron <i@aron.tw>
2025-01-15 18:09:58 +0800

    Add responsive button example page for demonstration

---
+++ site/app/[locale]/examples/responsive-button/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className='abs center-content flex full py:8x py:12x@sm'>
+            <button className="btn btn-md btn-sm@<2xs yellow touch-yellow">Submit</button>
+        </div>
+    )
+}
