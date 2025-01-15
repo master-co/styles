0fe5d274089d463cc209f0ec679f5db3c7cd069f
Aron <i@aron.tw>
2025-01-15 18:09:01 +0800

    Styles: Add rounded button variants for improved consistency

--- site/styles/btn.ts
+++ site/styles/btn.ts
@@ -4,11 +4,18 @@ export default {
     styles: {
         btn: {
             '': 'center-content inline-flex font:semibold outline-offset:-1',
-            'xs': 'font:12 h:6x px:2x px:3x.rounded r:4',
-            'sm': 'font:12 h:8x px:3x px:4x.rounded r:6',
-            'md': 'font:14 h:10x px:4x px:5x.rounded r:6',
-            'lg': 'font:16 h:12x px:5x px:6x.rounded r:8',
-            'xl': 'font:16 h:14x px:6x px:7x.rounded r:10'
+            'xs': 'font:12 h:6x px:2x r:4',
+            'sm': 'font:12 h:8x px:3x r:6',
+            'md': 'font:14 h:10x px:4x r:6',
+            'lg': 'font:16 h:12x px:5x r:8',
+            'xl': 'font:16 h:14x px:6x r:10',
+            'rounded': {
+                'xs': 'rounded font:12 h:6x px:3x',
+                'sm': 'rounded font:12 h:8x px:4x',
+                'md': 'rounded font:14 h:10x px:5x',
+                'lg': 'rounded font:16 h:12x px:6x',
+                'xl': 'rounded font:16 h:14x px:7x',
+            }
         }
     }
 } as Config
