2a44c30f1106925159bb8fe3283473e1c06a58d9
Aron <i@aron.tw>
2025-01-15 15:30:42 +0800

    Docs: Update descriptions for layers and enhance summary in cascade layers guide

--- site/app/[locale]/guide/cascade-layers/components/LayersDefault.tsx
+++ site/app/[locale]/guide/cascade-layers/components/LayersDefault.tsx
@@ -16,22 +16,22 @@ export default () => {
                 <tbody>
                     <tr>
                         <th>Base</th>
-                        <td>Normalize and reset global styles.</td>
+                        <td>Where the styles with <code>@base</code> are generated.</td>
                         <td className="white-space:nowrap"><InlineCode lang="css">{'@layer base { … }'}</InlineCode></td>
                     </tr>
                     <tr>
                         <th>Theme</th>
-                        <td>Where the <Link href="/guide/variables">variables</Link> are generated.</td>
+                        <td>Where the used <Link href="/guide/variables">variables</Link> are generated.</td>
                         <td className="white-space:nowrap"><InlineCode lang="css">{'@layer theme { … }'}</InlineCode></td>
                     </tr>
                     <tr>
                         <th>Preset</th>
-                        <td>Where the <code>@preset</code> styles are generated.</td>
+                        <td>Where the styles with <code>@preset</code> are generated.</td>
                         <td className="white-space:nowrap"><InlineCode lang="css">{'@layer preset { … }'}</InlineCode></td>
                     </tr>
                     <tr>
                         <th>Styles</th>
-                        <td>Where the <Link href="/guide/styles">styles</Link> are generated.</td>
+                        <td>Where the used <Link href="/guide/styles">styles</Link> are generated.</td>
                         <td className="white-space:nowrap"><InlineCode lang="css">{'@layer styles { … }'}</InlineCode></td>
                     </tr>
                     <tr>
