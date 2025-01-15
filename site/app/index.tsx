a2dc0e14973267f7d80c8d469fb43a47c1375fc2
Aron <i@aron.tw>
2025-01-15 23:16:49 +0800

    Remove roadmap link from navigation for clarity

--- site/app/index.tsx
+++ site/app/index.tsx
@@ -2,7 +2,6 @@
 
 import type { App } from 'internal/contexts/app'
 import { IconCompass, IconFileText, IconRoad, IconSourceCode } from '@tabler/icons-react'
-import roadmap from './[locale]/roadmap/metadata'
 import Logotype from 'internal/components/CSSLogotype'
 
 export default {
@@ -10,7 +9,7 @@ export default {
         { name: 'Guide', href: '/guide', Icon: IconCompass },
         { name: 'Reference', fullName: 'API Reference', href: '/reference', Icon: IconFileText },
         { name: 'Play', href: '/play', Icon: IconSourceCode },
-        { name: 'Roadmap', href: '/roadmap', Icon: IconRoad, date: roadmap.date },
+        // { name: 'Roadmap', href: '/roadmap', Icon: IconRoad, date: roadmap.date },
         // { name: 'Blog', href: '/blog', disabled: true, Icon: IconWriting },
         // { name: 'Components', href: '/components', Icon: IconComponents, disabled: true },
         // { name: 'Templates', href: '/templates', disabled: true }
