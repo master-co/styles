bb9f55bee5b55b35bd995f2f92d68b29c7eda715
Aron <i@aron.tw>
2025-01-15 18:08:51 +0800

    Docs: Revise styles guide with an overview and new examples for abstract styles

---
+++ site/app/[locale]/guide/styles/tests/apply-styles/master.css.js
@@ -0,0 +1,9 @@
+export default {
+    styles: {
+        btn: {
+            '': '… inline-flex font:semibold',
+            'sm': 'font:12 h:8x px:3x r:6',
+            'md': 'font:14 h:10x px:4x r:6',
+        }
+    }
+}
