841224cae65752dfc491f91a0c939b58081ef762
Aron <i@aron.tw>
2025-01-15 23:04:50 +0800

    Remove roadmap page

--- site/app/[locale]/roadmap/components/ProductRoadmap.tsx
+++
@@ -1,9 +0,0 @@
-export default () => (
-    <div className='grid-cols:13'>
-        <div>Master CSS v2</div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-)
