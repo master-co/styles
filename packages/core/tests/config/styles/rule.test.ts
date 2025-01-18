63c235147e32f261d3380d183c77f6a7b01fb31e
Aron <i@aron.tw>
2025-01-18 21:49:53 +0800

    Add initial test for MasterCSS rule generation

---
+++ packages/core/tests/config/styles/rule.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest'
+import { MasterCSS } from '../../../src'
+
+test('rule', () => {
+    const config = {
+        styles: {
+            btn: 'inline-flex bg:#000000'
+        }
+    }
+    const rule = new MasterCSS(config).generate('btn')[0]
+    expect(rule.fixedClass).toBe('btn')
+})
