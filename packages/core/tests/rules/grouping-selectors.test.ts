38a996d13ad03964749fcfd7883dd1dadece4d99
Aron <i@aron.tw>
2025-01-19 21:02:59 +0800

    Remove: grouping selectors test file

--- packages/core/tests/rules/grouping-selectors.test.ts
+++
@@ -1,7 +0,0 @@
-import { it, test, expect } from 'vitest'
-import { MasterCSS } from '../../src'
-
-test.concurrent('grouping selectors', () => {
-    expect(new MasterCSS().create('{fg:white;bg:black}_h1,_button')?.text).toBe('.\\{fg\\:white\\;bg\\:black\\}_h1\\,_button h1,.\\{fg\\:white\\;bg\\:black\\}_h1\\,_button button{color:rgb(255 255 255);background-color:rgb(0 0 0)}')
-    // expect(new MasterCSS().create('block:before,:after')?.text).toBe('.block\\:before\\,\\:after:before,.block\\:before\\,\\:after:after{display:block}')
-})
